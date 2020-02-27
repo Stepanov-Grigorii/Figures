8ef05219a8242f287dae7936d8f1659f2ea2e406
Stepanov-Grigorii <stegrand@yandex.ru>
2020-02-27 19:00:03 +0300

    Commit.6

--- src/com/company/lesson2/Main.java
+++ src/com/company/lesson2/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FiguresException {
+    public static void main(String[] args) {
         boolean flag = true;
         Scanner scanner = new Scanner(System.in);
         ArrayList<ClosedFigure> closedFigures = new ArrayList<>();
